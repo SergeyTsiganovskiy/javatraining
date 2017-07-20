eecca540fc1560022c1abf1bebb22191141fd69e
tsyganovskiy <tsyganovskiySS@gmail.com>
2017-07-20 20:50:04 +0300

    refactor rename packege

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactCreationTests extends TestBase{
+
+  @Test
+  public void testContactCreation() {
+    app.getNavigationHelper().gotoContactPage();
+    app.getContactHelper().initContactCreation();
+    app.getContactHelper().fillContactForm(new ContactData("John", "Smith", "SuperCompany", "Some text", "725-25-25", "0971111111", "test@gmail.com"));
+    app.getContactHelper().submitForm();
+    app.getNavigationHelper().gotoHomePage();
+  }
+}
