4b8201c77cc394e6fe8a19eec8f49321d2edca85
tsyganovskiy <tsyganovskiySS@gmail.com>
2017-09-10 22:29:53 +0300

    изменён build.gradle для модуля sandbox

--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -6,7 +6,25 @@ import org.testng.annotations.Test;
 public class SquareTests {
 
   @Test
-  public void testArea() {
+  public void testArea1() {
+    Square s = new Square(5);
+    Assert.assertEquals(s.area(), 25.0);
+  }
+
+  @Test
+  public void testArea2() {
+    Square s = new Square(5);
+    Assert.assertEquals(s.area(), 25.0);
+  }
+
+  @Test
+  public void testArea3() {
+    Square s = new Square(5);
+    Assert.assertEquals(s.area(), 25.0);
+  }
+
+  @Test
+  public void testArea4() {
     Square s = new Square(5);
     Assert.assertEquals(s.area(), 25.0);
   }
